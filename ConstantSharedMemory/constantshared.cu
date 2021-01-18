84261c550043e16c6fc0ff1f4fd6514285e69ef0
Alessioventuri <ale.venturi95@yahoo.it>
2021-01-18 13:21:00 +0100

    Fixed some errors and added relation

--- ConstantSharedMemory/constantshared.cu
+++ ConstantSharedMemory/constantshared.cu
@@ -14,53 +14,45 @@
 using namespace std::chrono;
 using namespace std;
 
-
-#define TILE_WIDTH 16  // 256 threads
+#define TILE_WIDTH 16 // 256 threads
 #define maskCols 5
 #define maskRows 5
 #define BLOCK_WIDTH (TILE_WIDTH + maskCols -1)
-#define clamp(x) (min(max((x), 0.0), 1.0))
 
-// mask in constant memory
+//mask in constant memory
 __constant__ float deviceMaskData[maskRows * maskCols];
-
 __global__ void ConstantSharedMemoryConvolution(float * InputImageData, const float *__restrict__ kernel,
 		float* outputImageData, int channels, int width, int height){
 
-	__shared__ float N_ds[BLOCK_WIDTH][BLOCK_WIDTH];  //block of image in shared memory
-	
+	__shared__ float N_ds[BLOCK_WIDTH][BLOCK_WIDTH];	//block of image in shared memory
+
+
 	// allocation in shared memory of image blocks
-	
 	int maskRadius = maskRows/2;
  	for (int k = 0; k <channels; k++) {
  		int dest = threadIdx.y * TILE_WIDTH + threadIdx.x; // flatten the 2D coordinates of the generic thread
- 		int destY = dest/BLOCK_WIDTH;   //row of shared memory (makes the inverse operation , in that it calculates the 2D coordinates )
- 		int destX = dest%BLOCK_WIDTH;	//col of shared memory (of the generica thread with respect to the shared memory area )	
- 		int srcY = blockIdx.y *TILE_WIDTH + destY - maskRadius; // index to fetch data from input image
- 		int srcX = blockIdx.x *TILE_WIDTH + destX - maskRadius; // index to fetch data from input image
- 		int src = (srcY *width +srcX) * channels + k;   // index of input image
- 		
- 		// When a thread is to load any input element, test if it is in the valid index range
- 		
+ 		int destY = dest/BLOCK_WIDTH;     //col of shared memory
+ 		int destX = dest%BLOCK_WIDTH;		//row of shared memory
+ 		int srcY = blockIdx.y *TILE_WIDTH + destY - maskRadius;  //row index to fetch data from input image
+ 		int srcX = blockIdx.x *TILE_WIDTH + destX - maskRadius;	//col index to fetch data from input image
  		if(srcY>= 0 && srcY < height && srcX>=0 && srcX < width)
- 			N_ds[destY][destX] = InputImageData[src];  // copy element of image in shared memory
+ 			N_ds[destY][destX] = InputImageData[(srcY *width +srcX) * channels + k]; // copy element of image in shared memory
  		else
  			N_ds[destY][destX] = 0;
+
 
  		dest = threadIdx.y * TILE_WIDTH+ threadIdx.x + TILE_WIDTH * TILE_WIDTH;
  		destY = dest/BLOCK_WIDTH;
 		destX = dest%BLOCK_WIDTH;
-		srcY = blockIdx.y * TILE_WIDTH + destY - maskRadius;
-		srcX = blockIdx.x * TILE_WIDTH + destX - maskRadius;
-		src = (srcY *width +srcX) * channels + k;
+		srcY = blockIdx.y *TILE_WIDTH + destY - maskRadius;
+		srcX = blockIdx.x *TILE_WIDTH + destX - maskRadius;
 		if(destY < BLOCK_WIDTH){
 			if(srcY>= 0 && srcY < height && srcX>=0 && srcX < width)
-				N_ds[destY][destX] = InputImageData[src];
+				N_ds[destY][destX] = InputImageData[(srcY *width +srcX) * channels + k];
 			else
 				N_ds[destY][destX] = 0;
 		}
 
-		//Barrier synchronization
  		__syncthreads();
 
 
@@ -69,7 +61,7 @@ __global__ void ConstantSharedMemoryConvolution(float * InputImageData, const fl
  		int y, x;
  		for (y= 0; y < maskCols; y++)
  			for(x = 0; x<maskRows; x++)
- 				Pvalue += N_ds[threadIdx.y + y][threadIdx.x + x] * deviceMaskData[y * maskCols + x];
+ 				Pvalue += N_ds[threadIdx.y + y][threadIdx.x + x] *deviceMaskData[y * maskCols + x];
 
  		y = blockIdx.y * TILE_WIDTH + threadIdx.y;
  		x = blockIdx.x * TILE_WIDTH + threadIdx.x;
@@ -78,7 +70,8 @@ __global__ void ConstantSharedMemoryConvolution(float * InputImageData, const fl
  		__syncthreads();
 
 
-	}
+ 	}
+
 }
  	
 void imageConvolutionConstantSharedMemory(const char* inputfilepath, const char* outputfilepath ){
@@ -86,54 +79,51 @@ void imageConvolutionConstantSharedMemory(const char* inputfilepath, const char*
 	int imgChannels;
 	int imgHeight;
 	int imgWidth;
-	Image* imgInput;
-	Image* imgOutput;
+	Image* inputImage;
+	Image* outputImage;
 	float* hostInputImageData;
 	float* hostOutputImageData;
 	float* deviceInputImageData;
 	float* deviceOutputImageData;
-	float* deviceMaskData;
 	float hostMaskData[maskRows * maskCols]={
 			0.06, 0.06, 0.06, 0.06, 0.06,
 			0.06, 0.06, 0.06, 0.06, 0.06,
 			0.06, 0.06, 0.06, 0.06, 0.06,
 			0.06, 0.06, 0.06, 0.06, 0.06,
 			0.06, 0.06, 0.06, 0.06, 0.06
-
 	};
 
 
-	imgInput = import_PPM(inputfilepath);
-	
-    imgWidth = img_getWidth(imgInput);
-    imgHeight = img_getHeight(imgInput);
-    imgChannels = img_getChannels(imgInput);
+	inputImage = import_PPM(inputfilepath);
 
-	imgOutput = Image_new(imgWidth, imgHeight, imgChannels);
+	imgWidth = img_getWidth(inputImage);
+	imgHeight = img_getHeight(inputImage);
+	imgChannels = img_getChannels(inputImage);
 
-    hostInputImageData = img_getData(imgInput);
-    hostOutputImageData = img_getData(imgOutput);
+	outputImage = Image_new(imgWidth, imgHeight, imgChannels);
+
+	hostInputImageData = img_getData(inputImage);
+	hostOutputImageData = img_getData(outputImage);
 
 	cudaDeviceReset();
-	
 	cudaMalloc((void **) &deviceInputImageData, imgWidth * imgHeight *
 			imgChannels * sizeof(float));
-			
 	cudaMalloc((void **) &deviceOutputImageData, imgWidth * imgHeight *
 			imgChannels * sizeof(float));
-			
 	cudaMemcpy(deviceInputImageData, hostInputImageData,
 			imgWidth * imgHeight * imgChannels * sizeof(float),
 			cudaMemcpyHostToDevice);
-	
-	// Copies "count" bytes from the memory area pointed to by hostMask to the memory area pointed to by "offset" bytes from 
-    // the start of symbol "deviceMask"
+
 	cudaMemcpyToSymbol(deviceMaskData, hostMaskData, maskRows * maskCols * sizeof(float));
 
+	float numberBlockXTiling = (float) imgWidth / TILE_WIDTH;
+	float numberBlockYTiling = (float) imgHeight / TILE_WIDTH;
 
-	dim3 dimGrid(ceil((float) imgWidth/TILE_WIDTH),
-			ceil((float) imgHeight/TILE_WIDTH));
-	dim3 dimBlock(TILE_WIDTH,TILE_WIDTH,1);
+	int numberBlockX = ceil(numberBlockXTiling);
+	int numberBlockY = ceil(numberBlockYTiling);
+
+	dim3 dimGrid(numberBlockX, numberBlockY);
+	dim3 dimBlock(TILE_WIDTH, TILE_WIDTH,1);
 
 
 	cout << "CONVOLUTION CONSTANT SHARED MEMORY" << endl;
@@ -152,7 +142,7 @@ void imageConvolutionConstantSharedMemory(const char* inputfilepath, const char*
 	cudaMemcpy(hostOutputImageData, deviceOutputImageData, imgWidth * imgHeight *
 			imgChannels * sizeof(float), cudaMemcpyDeviceToHost);
 
-	write_image(outputfilepath, imgOutput);
+	write_image(outputfilepath, outputImage);
 
 	cudaMemset(deviceInputImageData,0,imgWidth * imgHeight *
 				imgChannels * sizeof(float));
@@ -165,8 +155,8 @@ void imageConvolutionConstantSharedMemory(const char* inputfilepath, const char*
 	cudaFree(deviceOutputImageData);
 	cudaFree(deviceMaskData);
 
-	Image_delete(imgOutput);
-	Image_delete(imgInput);
+	Image_delete(outputImage);
+	Image_delete(inputImage);
 	
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ParallelComputing
+
+Implementation of 2D Convolution with Sequential method and Parallel Method using CUDA.
+In particular , you will find : 
+* Global and Global + Constant
+* Shared and Shared + Constant
+
+To read the images, I used the ppm format and C++ as support code.
